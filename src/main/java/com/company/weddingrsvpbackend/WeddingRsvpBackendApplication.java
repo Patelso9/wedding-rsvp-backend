5f0d5f186a003f7f78dd8575c6711eea290234be
Princess <sonali.patel.pkg@gmail.com>
2022-03-04 14:02:30 -0600

    t

---
+++ src/main/java/com/company/weddingrsvpbackend/WeddingRsvpBackendApplication.java
@@ -0,0 +1,13 @@
+package com.company.weddingrsvpbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeddingRsvpBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeddingRsvpBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/company/weddingrsvpbackend/WeddingRsvpBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.company.weddingrsvpbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeddingRsvpBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
