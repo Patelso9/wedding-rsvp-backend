c37289449987745d4178a79365fbfe3d83fb9ecc
Sanjay Gonsalves <snjgonsalves@gmail.com>
2022-03-05 07:34:34 -0800

    Updated Schema

--- rsvpSchema.sql
+++ rsvpSchema.sql
@@ -1,34 +1,41 @@
 create schema if not exists wedding_rsvp;
 use wedding_rsvp;
 create table if not exists event (
-    rsvp_id INT NOT NULL auto_increment primary key,
-    guest_id INT NOT NULL,
-    guest_name varchar(50) not null,
-    guest_email varchar(50) not null,
-    total_attending int not null,
+    event_id INT NOT NULL auto_increment primary key,
+    guestId INT NOT NULL,
+    guestName varchar(50) not null,
+    guestEmail varchar(50) not null,
+    totalAttending int not null,
     description varchar(50) not null
 );
 create table if not exists guest (
-    id int not null auto_increment primary key,
-    guest_name varchar(50) not null,
-    guest_email varchar(50) not null,
+    event_id int not null auto_increment primary key,
+    id int NOT NULL,
+    name varchar(50) not null,
+    email varchar(50) not null,
     attending BOOLEAN not null
 );
 
 describe event;
 describe guest;
 
-INSERT INTO guest(guest_name,guest_email,attending) VALUE("William Short","william@mail.com",TRUE);
-INSERT INTO guest(guest_name,guest_email,attending) VALUE("Richard Lagahid","richard@mail.com",TRUE);
-INSERT INTO guest(guest_name,guest_email,attending) VALUE("Jhon Gallera","john@mail.com",TRUE);
-INSERT INTO guest(guest_name,guest_email,attending) VALUE("Brian Rey","brian@mail.com",TRUE);
-INSERT INTO guest(guest_name,guest_email,attending) VALUE("Mick Miller","mic@mail.com",TRUE);
+INSERT INTO guest(id,name,email,attending) VALUE(1,"William Short","william@mail.com",TRUE);
+INSERT INTO guest(id,name,email,attending) VALUE(2,"Richard Lagahid","richard@mail.com",TRUE);
+INSERT INTO guest(id,name,email,attending) VALUE(3,"Jhon Gallera","john@mail.com",TRUE);
+INSERT INTO guest(id,name,email,attending) VALUE(4,"Brian Rey","brian@mail.com",TRUE);
+INSERT INTO guest(id,name,email,attending) VALUE(5,"Mick Miller","mic@mail.com",TRUE);
+
+INSERT INTO guest(id,name,email,attending) VALUE(1,"Bobby Brown","bobby@mail.com",TRUE);
+INSERT INTO guest(id,name,email,attending) VALUE(2,"Ray Charles","rey@mail.com",TRUE);
+INSERT INTO guest(id,name,email,attending) VALUE(3,"Steve Vai","Steve@mail.com",TRUE);
+INSERT INTO guest(id,name,email,attending) VALUE(4,"Brix Mcnight","brix@mail.com",TRUE);
+INSERT INTO guest(id,name,email,attending) VALUE(5,"Tim Allen","tim@mail.com",TRUE);
 
 select * from event;
 select * from guest;
 
 
 /* Foreign Keys:  */
-alter table event add constraint fk_event_guest foreign key (rsvp_id) references guest(id);
+alter table event add constraint fk_event_guest foreign key (event_id) references guest(event_id);
 
 ALTER TABLE event DROP FOREIGN KEY fk_event_guest;
