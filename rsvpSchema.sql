e3ca700e91cd55c17bea198fe30326e7c53f9e02
Sanjay Gonsalves <snjgonsalves@gmail.com>
2022-03-05 08:16:13 -0800

    Added new schema and reduced variables to minimal

--- rsvpSchema.sql
+++ rsvpSchema.sql
@@ -1,16 +1,15 @@
 create schema if not exists wedding_rsvp;
 use wedding_rsvp;
 create table if not exists event (
-    event_id INT NOT NULL auto_increment primary key,
-    guestId INT NOT NULL,
-    guestName varchar(50) not null,
-    guestEmail varchar(50) not null,
+    event_id int not null auto_increment primary key,
+    guest_name varchar(50) not null,
+	guest_email varchar(50) not null,
     totalAttending int not null,
     description varchar(50) not null
 );
 create table if not exists guest (
-    event_id int not null auto_increment primary key,
-    id int NOT NULL,
+    guest_id int not null auto_increment primary key,
+    event_id int not null,
     name varchar(50) not null,
     email varchar(50) not null,
     attending BOOLEAN not null
@@ -19,23 +18,12 @@ create table if not exists guest (
 describe event;
 describe guest;
 
-INSERT INTO guest(id,name,email,attending) VALUE(1,"William Short","william@mail.com",TRUE);
-INSERT INTO guest(id,name,email,attending) VALUE(2,"Richard Lagahid","richard@mail.com",TRUE);
-INSERT INTO guest(id,name,email,attending) VALUE(3,"Jhon Gallera","john@mail.com",TRUE);
-INSERT INTO guest(id,name,email,attending) VALUE(4,"Brian Rey","brian@mail.com",TRUE);
-INSERT INTO guest(id,name,email,attending) VALUE(5,"Mick Miller","mic@mail.com",TRUE);
-
-INSERT INTO guest(id,name,email,attending) VALUE(6,"Bobby Brown","bobby@mail.com",TRUE);
-INSERT INTO guest(id,name,email,attending) VALUE(7,"Ray Charles","rey@mail.com",TRUE);
-INSERT INTO guest(id,name,email,attending) VALUE(8,"Steve Vai","Steve@mail.com",TRUE);
-INSERT INTO guest(id,name,email,attending) VALUE(9,"Brix Mcnight","brix@mail.com",TRUE);
-INSERT INTO guest(id,name,email,attending) VALUE(10,"Tim Allen","tim@mail.com",TRUE);
 
 select * from event;
 select * from guest;
 
+/* Foreign Keys */
+alter table guest add constraint fk_guest_event foreign key (event_id) references event(event_id);
 
-/* Foreign Keys:  */
-alter table event add constraint fk_event_guest foreign key (event_id) references guest(event_id);
-
+/*Drop Foregin Keys */
 ALTER TABLE event DROP FOREIGN KEY fk_event_guest;
--- src/main/java/com/company/weddingrsvpbackend/repository/GuestRepository.java
+++
@@ -1,9 +0,0 @@
-package com.company.weddingrsvpbackend.repository;
-
-import com.company.weddingrsvpbackend.dto.Guests;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface GuestRepository extends JpaRepository<Guests, Integer> {
-}
