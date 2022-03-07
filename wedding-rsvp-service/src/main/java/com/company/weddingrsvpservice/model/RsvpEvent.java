d369156034b480860bfd1da51583c95c74a5180a
Sanjay Gonsalves <snjgonsalves@gmail.com>
2022-03-07 11:08:08 -0800

    removed On to many and set variables to minimal

---
+++ wedding-rsvp-service/src/main/java/com/company/weddingrsvpservice/model/RsvpEvent.java
@@ -0,0 +1,88 @@
+package com.company.weddingrsvpservice.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.Objects;
+
+@Entity
+@JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
+@Table(name = "event")
+public class RsvpEvent implements Serializable {
+    @Id
+    @Column(name = "event_id" )
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private  int id;
+
+    private String guestName;
+    private String guestEmail;
+    private int totalAttending;
+    private String description;
+
+    public RsvpEvent() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getGuestName() {
+        return guestName;
+    }
+
+    public void setGuestName(String guestName) {
+        this.guestName = guestName;
+    }
+
+    public String getGuestEmail() {
+        return guestEmail;
+    }
+
+    public void setGuestEmail(String guestEmail) {
+        this.guestEmail = guestEmail;
+    }
+
+    public int getTotalAttending() {
+        return totalAttending;
+    }
+
+    public void setTotalAttending(int totalAttending) {
+        this.totalAttending = totalAttending;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        RsvpEvent rsvpEvent = (RsvpEvent) o;
+        return id == rsvpEvent.id && totalAttending == rsvpEvent.totalAttending && Objects.equals(guestName, rsvpEvent.guestName) && Objects.equals(guestEmail, rsvpEvent.guestEmail) && Objects.equals(description, rsvpEvent.description);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, guestName, guestEmail, totalAttending, description);
+    }
+
+    @Override
+    public String toString() {
+        return "RsvpEvent{" +
+                "id=" + id +
+                ", guestName='" + guestName + '\'' +
+                ", guestEmail='" + guestEmail + '\'' +
+                ", totalAttending=" + totalAttending +
+                ", description='" + description + '\'' +
+                '}';
+    }
+}
---
+++ wedding-rsvp-service/src/main/java/com/company/weddingrsvpservice/model/RsvpGuests.java
@@ -0,0 +1,92 @@
+package com.company.weddingrsvpservice.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import javax.persistence.*;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import java.io.Serializable;
+import java.util.Objects;
+
+@Entity
+@JsonIgnoreProperties({"hibernateLazyIntializer", "handler"})
+@Table(name="guest")
+public class RsvpGuests extends RsvpEvent{
+
+    private int id;
+    private int guest_event_id;
+    private String name;
+    private String email;
+    private Boolean attending;
+
+    public RsvpGuests() {
+    }
+
+    @Override
+    public int getId() {
+        return id;
+    }
+
+    @Override
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getGuest_event_id() {
+        return guest_event_id;
+    }
+
+    public void setGuest_event_id(int guest_event_id) {
+        this.guest_event_id = guest_event_id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Boolean getAttending() {
+        return attending;
+    }
+
+    public void setAttending(Boolean attending) {
+        this.attending = attending;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        if (!super.equals(o)) return false;
+        RsvpGuests that = (RsvpGuests) o;
+        return id == that.id && guest_event_id == that.guest_event_id && Objects.equals(name, that.name) && Objects.equals(email, that.email) && Objects.equals(attending, that.attending);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(super.hashCode(), id, guest_event_id, name, email, attending);
+    }
+
+    @Override
+    public String toString() {
+        return "RsvpGuests{" +
+                "id=" + id +
+                ", guest_event_id=" + guest_event_id +
+                ", name='" + name + '\'' +
+                ", email='" + email + '\'' +
+                ", attending=" + attending +
+                '}';
+    }
+}
